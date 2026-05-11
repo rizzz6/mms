9594ef73a61694b8bb909f32b8918a74dfa81485
rizzz6 <rizvialam2005@gmail.com>
2026-05-11 16:02:58 +0530

    fix 9

--- open-next.config.ts
+++ open-next.config.ts
@@ -4,22 +4,14 @@ const config = {
       wrapper: "cloudflare-node",
       converter: "edge",
       proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
     },
-    minify: false,
   },
-  edgeExternals: ["node:crypto"],
   middleware: {
     external: true,
     override: {
       wrapper: "cloudflare-edge",
       converter: "edge",
       proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
     },
   },
 };
